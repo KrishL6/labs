20d5dfb35d2336f145a20cfbdabf3639c940616f
KRISH PATEL <26543842@edgehill.ac.uk>
2025-10-21 14:58:02 +0100

    Feat: >> SQUARE

--- week3_lab.py
+++ week3_lab.py
@@ -5,6 +5,9 @@ for index in range(2, 21, 2):
     print (index)
 """
 
+# This code calculates the total cost from a list of costs
+
+"""
 costs = [15.00, 12.50, 3.75, 40.25, 6.99]
 
 total_cost = 0
@@ -14,3 +17,12 @@ for index in range(len(costs)):
 
 text = f"The total cost is: {total_cost:.2f}"
 print(text)
+"""
+# code that prints a 5x5 square of asterisks (*)
+
+for index in range(5):
+    print()
+    for index in range(5):
+        print('*', end=' ')
+
+
